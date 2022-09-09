9a43e03afef75be0286607d903876685350685fd
levinworld <masabaivan@outlook.com>
2022-09-09 16:48:22 -0700

    Positive better than negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,10 +4,7 @@
 
 /**
  * main - Entry point
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-
+*/
 /**
  *  * main - determine if number is positive or negative
  *   * Description: program will assign a random number to the variable 
@@ -16,17 +13,17 @@
 
 int main(void)
 {
-		int n;
+	int n;
 
-			srand(time(0));
-				n = rand() - RAND_MAX / 2;
-					if (n == 0)
-								printf("%d is zero\n", n);
-						else if (n < 0)
-									printf("%d is negative\n", n);
-							else
-										printf("%d is positive\n", n);
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n == 0)
+	printf("%d is zero\n", n);
+	else if (n < 0)
+	printf("%d is negative\n", n);
+	else
+	printf("%d is positive\n", n);
 
-								return (0);
+	return (0);
 }
 
