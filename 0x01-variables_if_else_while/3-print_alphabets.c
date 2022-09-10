4e68487b732ac13f5ecd0d344cdc2420f4889f75
levinworld <masabaivan@outlook.com>
2022-09-09 19:18:42 -0700

    alphabet 2

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -14,11 +14,11 @@ int main(void)
 	for (lower_case = 'a'; lower_case <= 'z'; lower_case++)
 		{
 			putchar(lower_case);
-										}
+		}
 	for (upper_case = 'A'; upper_case <= 'Z'; upper_case++)
 		{
 			putchar(upper_case);
-											}
+		}
 			putchar('\n');
 	return (0);
 }
