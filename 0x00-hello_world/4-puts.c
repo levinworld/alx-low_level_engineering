4a795198f2106012e508cce6f09a55b8f8519422
levinworld <masabaivan@outlook.com>
2022-09-11 14:36:48 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * Description: main - prints out a string to stdout
+ * 
+ * Return: 0 if Success
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+}
