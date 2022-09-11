35b778e0a80cba9ec749113c68bca7861e8add17
levinworld <masabaivan@outlook.com>
2022-09-11 04:50:08 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ *  * main - determine if number is positive or negative
+ *   * Description: program will assign a random number to the variable 
+ *    * Return: Always 0 (Success)
+ *     */
+
+int main(void)
+{
+		int d;
+
+		for (d = "0"; d <= '9'; d++)
+		    putchar(d);
+		putchar('\n');
+		
+		return (0);
+}
