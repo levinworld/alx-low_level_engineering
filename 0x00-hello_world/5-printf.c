9c9a172d6ffe54124a70cb7fe05dc94ae95eeb62
levinworld <masabaivan@outlook.com>
2022-09-11 14:47:18 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * Description: prints out a string to stdout.
+ * Return: 0 if Success
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
