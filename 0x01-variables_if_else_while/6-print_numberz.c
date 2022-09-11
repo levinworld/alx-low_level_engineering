291f7bb72946315d42e5d46e18108e71af57b070
levinworld <masabaivan@outlook.com>
2022-09-11 06:27:24 -0700

    6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -5,15 +5,14 @@
  * main - Entry point
  *
  * Return: Always 0 (Success)
-*/
+ */
 int main(void)
 {
-		int d;
+	int d;
 
-		for (d = '0'; d <= '9'; d++)
+	for (d = '0'; d <= '9'; d++)
+		putchar(d);
+	putchar('\n');
 
-		    putchar(d);
-			putchar('\n');
-
-		return (0);
+	return (0);
 }
