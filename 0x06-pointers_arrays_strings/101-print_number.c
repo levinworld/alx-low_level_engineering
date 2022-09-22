5af92699c454d7d6d46cb5fa21d3a2ab38b7c07b
levinworld <masabaivan@outlook.com>
2022-09-22 13:10:37 -0700

    101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -3,23 +3,19 @@
 /**
  * print_number - Prints an integer.
  * @n: The integer to be printed.
- *
- * Return: Nothing
  */
 void print_number(int n)
 {
-	unsigned int x;
+	unsigned int num = n;
 
 	if (n < 0)
 	{
 		_putchar('-');
-		num *= -1;
+		num = -num;
 	}
 
-	x = n;
+	if ((num / 10) > 0)
+		print_number(num / 10);
 
-	if (x / 10)
-		print_number(x / 10);
-
-	_putchar(x % 10 + '0');
+	_putchar((num % 10) + '0');
 }
